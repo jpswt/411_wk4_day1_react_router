3dd1c3265c38c43c7e2fa259c2788c60052a3558
jpswt <joel_swt@yahoo.com>
2022-08-11 19:25:34 -0500

    Updates

---
+++ precache-manifest.a1ba5c3db674edfa91ef115377bd907c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf444d7150166824727b5e9d7147361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72c10d6b0a96b941499",
+    "url": "/static/css/main.05ad2c06.chunk.css"
+  },
+  {
+    "revision": "2be88c3ff9846d00bef2",
+    "url": "/static/js/2.4700f7c2.chunk.js"
+  },
+  {
+    "revision": "e72c10d6b0a96b941499",
+    "url": "/static/js/main.19d7da3e.chunk.js"
+  },
+  {
+    "revision": "bc03988ff43f217e7fe9",
+    "url": "/static/js/runtime~main.484bd350.js"
+  }
+]);
